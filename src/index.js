2babb2a82a62ca2479ff7a0b3b9a79ad5e917c56
Patricia Bird <patriciabird@Patricias-MacBook-Air.local>
2021-12-08 14:51:25 -0500

    Add myName export and use import in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import _ from 'lodash';
+import myName from './myName';
 
-function component() {
+function componentName() {
     const element = document.createElement('div');
-  
+
+    // use your function!
+    element.innerHTML = myName('Cody');
+    return element;
+}
+
+function componentLoDash() {
+    const element = document.createElement('div');
+
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+
     return element;
-  }
-  
-  document.body.appendChild(component());
+}
+
+document.body.appendChild(componentName());
+document.body.appendChild(componentLoDash());
